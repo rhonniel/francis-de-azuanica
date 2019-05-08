2aff3f2257b1821ac45509942728686e7175417b
Ruo07 <23141612+rhonniel@users.noreply.github.com>
2019-05-07 20:27:16 -0400

    version inicial

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingTate;
+   
+
+
+
+    State state;
+    int items = 0;
+
+	// Use this for initialization
+	void Start () {
+        state = startingTate;
+        textComponent.text = state.GetStateStory();
+     
+     
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ManageState();
+        
+	}
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+       
+
+        if (state.stateEnd())
+        {
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                state = nextStates[items];
+            }
+                
+            
+        }
+        else
+        {
+            for (int i = 0; i < nextStates.Length; i++)
+            {
+
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                {
+                    if (state.stateInit())
+                    {
+                        items = 0;
+                    }
+
+                    if (state.stateHaveItem())
+                    {
+                        items++;
+                    }
+                    state = nextStates[i];
+                }
+
+            }
+        }
+
+        
+     
+
+        textComponent.text = state.GetStateStory();
+    }
+
+}
---
+++ Assets/State.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*este comando de abajo hace que puedas crear una nueva opcion de objeto
+ para el menu de Unity Asset, con la instruccion de abajo se añade la opcion
+ de crear un objeto tipo State */
+[CreateAssetMenu(menuName ="State")]
+
+public class State : ScriptableObject {
+
+    /*por lo visto cuando utiliza los corchetes estas de algun podo comunicandote 
+     con la interfaz de unity, esta manipulando la forma  en que unity 
+     representara el campo*/
+
+     /*este caso se esta representado al objeto como une text area, 
+      el primer parametro representa el tamaño que sera visible y el otro representa
+      el tamaño del scroll
+        
+         */
+   
+    
+
+    [TextArea(14,10)] [SerializeField] string storyText;
+
+    [TextArea(7, 7)] [SerializeField] string type;
+
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory() {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+    public bool stateHaveItem()
+    {
+        if(type == null)
+        {
+            return false;
+        }
+
+        type = type.Trim();
+        if (type == "I")
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+        
+    }
+
+    public bool stateInit()
+    {
+
+        if (type == null)
+        {
+            return false;
+        }
+        type = type.Trim();
+        if (type == "A")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+
+    public bool stateEnd()
+    {
+
+        if (type == null)
+        {
+            return false;
+        }
+
+        type = type.Trim();
+        if (type == "E")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
+    }
+
+}
